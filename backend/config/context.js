2527b1141f9b8c3b9a884b8d57483cff7a2c08fc
edumoritz <eduardo.moritz@hotmail.com>
2020-03-14 09:40:22 -0300

    simula login

--- backend/config/context.js
+++ backend/config/context.js
@@ -1,4 +1,6 @@
-module.exports = ({ req }) => {
+module.exports = async ({ req }) => {
+  // Em desenvolvimento
+  await require('./simularUsuarioLogado')(req)
+
   const auth = req.headers.authorization
-  console.log(auth)
 }
